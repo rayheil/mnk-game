db3a318561b928bb48841250de4194f0127fba71
Ray Heil <68617358+raymondheil@users.noreply.github.com>
2023-03-07 21:08:22 -0600

    Update to AI code, still nowhere near ready

--- src/tictactoe/AI/TicTacToeAI.java
+++ src/tictactoe/AI/TicTacToeAI.java
@@ -1,5 +1,6 @@
 package tictactoe.AI;
 
+import gamecore.LINQ.LINQ;
 import gamecore.datastructures.vectors.Vector2i;
 import tictactoe.model.ITicTacToeBoard;
 import tictactoe.model.Player;
@@ -7,7 +8,7 @@ import tictactoe.model.Player;
 public class TicTacToeAI implements ITicTacToeAI {
 
 	/**
-	 * Default constructor that creates an AI of difficulty 5.
+	 * Default constructor that creates an AI of difficulty 5. Why not?
 	 */
 	public TicTacToeAI(Player player)
 	{
@@ -19,9 +20,16 @@ public class TicTacToeAI implements ITicTacToeAI {
 	 */
 	public TicTacToeAI(Player player, int difficulty)
 	{
+		if (player == null)
+			throw new NullPointerException();
+		
+		if (difficulty < 1 || difficulty > 10)
+			throw new IllegalArgumentException("Difficulty must be between 1 and 10 (inclusive)");
+		
 		Player = player;
 		Difficulty = difficulty;
 		// TODO the rest of the dang constructor lol
+		// is it even a thing that happens? this thing has so few required attributes.
 	}
 	
 	@Override
@@ -29,12 +37,20 @@ public class TicTacToeAI implements ITicTacToeAI {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/**
+	 * Return an iterator containing all the playable states from a given board state
+	 * (all those cells that are empty).
+	 * @param board The current board state.
+	 * @return Iterable over all empty cells in the board
+	 */
+	protected Iterable<Vector2i> GetChildStates(ITicTacToeBoard board) {
+		return LINQ.Where(board.IndexSet(), t -> board.IsCellEmpty(t));
+	}
 
 	@Override
-	public Player GetPlayer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Player GetPlayer() 
+	{return Player;}
 
 	/**
 	 * The difficulty of this AI.
